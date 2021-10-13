ecdf3276c33b7335a2339bcc27c754a5c3d0a6b2
sachintha00 <sachintha0603@gmail.com>
2021-10-13 07:52:57 +0530

    Update MainActivity.java

--- app/src/main/java/com/smd/cameraultra/MainActivity.java
+++ app/src/main/java/com/smd/cameraultra/MainActivity.java
@@ -2,15 +2,20 @@ package com.smd.cameraultra;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraX;
+import androidx.camera.core.Preview;
 import androidx.camera.core.PreviewConfig;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.content.pm.PackageManager;
+import android.opengl.EGLExt;
+import android.opengl.Matrix;
 import android.os.Bundle;
 import android.util.Rational;
 import android.util.Size;
+import android.view.Surface;
 import android.view.TextureView;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,6 +50,53 @@ public class MainActivity extends AppCompatActivity {
         PreviewConfig pConfig = new PreviewConfig.Builder()
                 .setTargetAspectRatio(aspectRetio)
                 .setTargetResolution(screen).build();
+        Preview preview = new Preview(pConfig);
+
+        preview.setOnPreviewOutputUpdateListener(
+                new Preview.OnPreviewOutputUpdateListener() {
+                    @Override
+                    public void onUpdated(Preview.PreviewOutput output) {
+                        ViewGroup parent = (ViewGroup) textureView.getParent();
+                        parent.removeView(textureView);
+                        parent.addView(textureView);
+
+                        textureView.setSurfaceTexture(output.getSurfaceTexture());
+                        updateTranform();
+                    }
+                }
+        );
+    }
+
+    private void updateTranform() {
+        Matrix matrix = new Matrix();
+        float w = textureView.getMeasuredWidth();
+        float h = textureView.getMeasuredHeight();
+
+        float cx = w/2f;
+        float cy = h/2f;
+
+        int rotaionDgr;
+        int rotation = (int) textureView.getRotation();
+
+        switch (rotation){
+            case Surface.ROTATION_0:
+                rotaionDgr = 0;
+                break;
+            case Surface.ROTATION_90:
+                rotaionDgr = 90;
+                break;
+            case Surface.ROTATION_180:
+                rotaionDgr = 180;
+                break;
+            case Surface.ROTATION_270:
+                rotaionDgr = 270;
+                break;
+            default:
+                return;
+        }
+
+//        matrix.postRotate((float)rotaionDgr, cx,cy);
+//        textureView.setTransform(mx);
     }
 
     private boolean allPermissionGranted() {
