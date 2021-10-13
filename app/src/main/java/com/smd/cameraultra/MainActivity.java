ec22e144518ffd1b5312dd361b462968b8765d2f
sachintha00 <sachintha0603@gmail.com>
2021-10-13 07:11:29 +0530

    Update MainActivity.java

--- app/src/main/java/com/smd/cameraultra/MainActivity.java
+++ app/src/main/java/com/smd/cameraultra/MainActivity.java
@@ -1,8 +1,10 @@
 package com.smd.cameraultra;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.os.Bundle;
+import android.view.TextureView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +14,25 @@ public class MainActivity extends AppCompatActivity {
             "android.permission.WRITE_EXTERNAL_STORAGE" +
             ""};
 
+    TextureView textureView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textureView = (TextureView)findViewById(R.id.view_finder);
+        if(allPermissionGranted()){
+            startCamera();
+        }
+        else{
+            ActivityCompat.requestPermissions(this,REQUIRED_PERMISSION,REQUEST_CODE_PERMISSIONS);
+        }
+    }
+
+    private void startCamera() {
+    }
+
+    private boolean allPermissionGranted() {
     }
 }
